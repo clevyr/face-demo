e4f67d15f4bc74475b8052da8c235f5e6f4b5959
Gabe Cook <gabe@clevyr.com>
2020-05-13 14:08:38 -0500

    :wrench: Enable Blazor DetailedErrors in development environments

--- WebServer/App/Startup.cs
+++ WebServer/App/Startup.cs
@@ -33,19 +33,27 @@ namespace FaceIdentifier
 
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            HostingEnvironment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddServerSideBlazor().AddCircuitOptions(o =>
+            {
+                if (HostingEnvironment.IsDevelopment())
+                {
+                    o.DetailedErrors = true;
+                }
+            });
 
 
             AppContext.SetSwitch(
